cb8f48b90ed2f6972c6ac0edd0457e2dcad562f3
Alex Reinking <alex.reinking@gmail.com>
2021-06-06 21:32:55 -0700

    Teach Findpybind11 to search pip install dirs

--- cmake/Findpybind11.cmake
+++ cmake/Findpybind11.cmake
@@ -1,7 +1,20 @@
 set(pybind11_VERSION "${${CMAKE_FIND_PACKAGE_NAME}_FIND_VERSION}")
 
+foreach (py IN ITEMS Python3 Python)
+    if (${py}_EXECUTABLE)
+        execute_process(COMMAND ${${py}_EXECUTABLE} -m site --user-site
+                        OUTPUT_VARIABLE ${py}_USERSITE
+                        OUTPUT_STRIP_TRAILING_WHITESPACE
+                        RESULT_VARIABLE code)
+        if (NOT code EQUAL 0)
+            # User has disabled pip
+            set(${py}_USERSITE "")
+        endif ()
+    endif ()
+endforeach ()
+
 find_package(pybind11 "${pybind11_VERSION}" QUIET
-             HINTS ${Python_SITELIB} ${Python3_SITELIB})
+             HINTS ${Python3_SITELIB} ${Python_SITELIB} ${Python3_USERSITE} ${Python_USERSITE})
 
 if (NOT pybind11_FOUND)
     include(FetchContent)
